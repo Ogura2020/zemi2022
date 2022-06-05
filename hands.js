1c73b7edd762468728d7babd0b48faf652996cad
Ogura2020 <cocoanapoli@gmail.com>
2022-06-06 02:32:54 +0900

    ジャスチャーに反応してイラストが変わるように変更しました。 canvasを非表示にしました。大きさも変更。

--- hands.js
+++ hands.js
@@ -1,7 +1,9 @@
 const videoElement = document.getElementsByClassName('input_video')[0];
+videoElement.style.display = "none";
 const canvasElement = document.getElementsByClassName('output_canvas')[0];
 const canvasCtx = canvasElement.getContext('2d');
 
+var img_src = new Array("image/maloik_sign_sdw.png","image/maloik_sign1.png","image/ok_sign_sdw.png","image/ok_sign1.png","image/kitune_sdw.png","image/kitune1.png");
 let i = 0;
 let isGestureDetected = false;  // ジェスチャーを検出したかのフラグ
 
@@ -21,8 +23,8 @@ const camera = new Camera(videoElement, {
   onFrame: async () => {
     await hands.send({image: videoElement});
   },
-  width: 540,
-  height: 320
+  width: 768,
+  height: 432
 });
 camera.start();
 
@@ -46,7 +48,7 @@ function getHand(results) {
       const hand  = results.multiHandLandmarks;
       const hand1 = results.multiHandLandmarks[0] ;
       const hand2 = results.multiHandLandmarks[1];
-      //console.log(hand1[8].y,hand1[6].y);
+      //console.log(hand);
 /*
       if (!isGestureDetected && (hand1[6].y < hand1[8].y > hand1[7].y) && (hand1[10].y < hand1[12].y > hand1[11].y) && (0.02 < Math.abs(hand1[8].x - hand1[12].x) > 0.07)) {
         function counter(){
@@ -59,32 +61,36 @@ function getHand(results) {
       }
 */
       if (!isGestureDetected && (hand1[12].y > hand1[10].y) && (hand1[16].y > hand1[14].y) && (hand1[8].y < hand1[10].y) && (hand1[20].y < hand1[10].y) && (hand1[4].y < hand1[11].y)) {
-        function counter(){
-          i++;
-          document.getElementById("press-button").innerHTML = i;
+        function change(){
+          document.getElementById("img1").src=img_src[1];
         }
-        counter();
-        console.log("🤘");
-        onGestureDetected();
+        change();
+        //console.log("🤘");
+        //onGestureDetected();
+      } else{
+        document.getElementById("img1").src=img_src[0];
       }
 
       if (!isGestureDetected && Math.abs(hand1[4].x - hand1[8].x) < 0.03 && Math.abs(hand1[4].y - hand1[8].y) < 0.03){
-        function counter(){
-          i--;
-          document.getElementById("press-button").innerHTML = i;
+        function change(){
+          document.getElementById("img2").src=img_src[3];
         }
-        counter();
-        console.log("👌");
-        onGestureDetected();
+        change();
+        //console.log("👌");
+        //onGestureDetected();
+      } else{
+        document.getElementById("img2").src=img_src[2];
       }
 
       if (!isGestureDetected && Math.abs(hand1[4].x - hand1[12].x) < 0.05 && Math.abs(hand1[4].y - hand1[12].y) < 0.05 && Math.abs(hand1[4].x - hand1[16].x) < 0.05 && Math.abs(hand1[4].y - hand1[16].y) < 0.05){
-        function counter(){
-          document.getElementById("press-button").innerHTML = i = 0;
+        function change(){
+          document.getElementById("img3").src=img_src[5];
         }
-        counter();
-        console.log("🤟");
-        onGestureDetected();
+        change();
+        //console.log("🤟");
+        //onGestureDetected();
+      } else{
+        document.getElementById("img3").src=img_src[4];
       }
       /*
       if ((hand1[6].y < hand1[8].y > hand1[7].y) && (hand1[10].y < hand1[12].y > hand1[11].y) && (0.03 < Math.abs(hand1[8].x - hand1[12].x) > 0.07) && i == 0) {
