2297c5d83a121401e4dd715b3409df8ba8843e7f
Ogura2020 <cocoanapoli@gmail.com>
2022-09-19 01:35:36 +0900

    文字ツールの修正、前面、背面に移動・取り消し・元に戻す・コピー・貼り付けを追加しました。

--- illustrator_handUI.py
+++ illustrator_handUI.py
@@ -73,7 +73,6 @@ def camera():
     #image.flags.writeable = True
     #image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     
-    landmark_list = ['WRIST', 'THUMP_CMC', 'THUMB_MCP', 'THUMB_IP', 'THUMB_TIP', 'INDEX_FINGER_MCP', 'INDEX_FINGER_PIP', 'INDEX_FINGER_DIP', 'INDEX_FINGER_TIP', 'MIDDLE_FINGER_MCP', 'MIDDLE_FINGER_PIP', 'MIDDLE_FINGER_DIP', 'MIDDLE_FINGER_TIP', 'RING_FINGER_MCP', 'RING_FINGER_PIP', 'RING_FINGER_DIP', 'RING_FINGER_TIP', 'PINKY_MCP', 'PINKY_PIP', 'PINKY_DIP', 'PINKY_TIP']
     yuX = []
     yuY = []
     if results.multi_hand_landmarks:
@@ -81,7 +80,7 @@ def camera():
        for i in range(21):
          yuX.append(hand_landmarks.landmark[i].x * image_width)
          yuY.append(hand_landmarks.landmark[i].y * image_height)
-         #print(yuX[0],yuY[0])
+         #print(hand_landmarks.landmark[4])
       X=(hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width)
       Y=(hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height)
 
@@ -93,32 +92,49 @@ def camera():
         pg.hotkey("p")
         time.sleep(1)
         print("ペンツール")
-      if yuY[8] < yuY[6] and yuY[8] < yuY[12] and yuY[8] < yuY[16] and yuY[8] < yuY[20] and abs(yuX[4]-yuX[12])<15:
+      if yuY[8] < yuY[6] and yuY[8] < yuY[12] and yuY[8] < yuY[16] and yuY[18] < yuY[20] and abs(yuX[4]-yuX[12])<13:
         #pg.hotkey("v")
         time.sleep(1)
         print("選択ツール")
-      if yuY[8] < yuY[6] and yuY[8] < yuY[12] and yuY[8] < yuY[16] and yuY[8] < yuY[20] and np.linalg.norm(yuX[4]-yuX[10]) > 25:
+      if yuY[8] < yuY[6] and yuY[8] < yuY[12] and yuY[8] < yuY[16] and yuY[18] < yuY[20] and 15 < np.linalg.norm(yuX[4]-yuX[10]) > 25:
         #pg.hotkey("v")
         time.sleep(1)
         print("ダイレクト選択ツール")
-      if yuY[8] > yuY[6] and yuY[12] > yuY[10] and yuY[16] > yuY[14] and yuY[4] < yuY[6] and yuY[20] < yuY[10]:
+      if yuX[8] > yuX[6] and yuX[12] > yuY[10] and 10 < np.linalg.norm(yuX[4]-yuX[10]) > 20 and yuX[8] > yuX[16] and yuX[12] > yuX[16]:
         #pg.hotkey("t")
         time.sleep(1)
         print("文字ツール")
-        
-      if yuX[8] > yuX[6] and yuX[12] > yuX[10] and yuX[16] > yuX[14] and yuX[20] > yuX[18] and yuY[4] < yuY[6]:
-        #pg.hotkey("t")
+      #if yuX[8] < yuX[6] and yuX[12] < yuX[10] and yuX[16] < yuX[14] and yuX[20] < yuX[18] and yuY[4] < yuY[0]:
+        #pg.hotkey("shift","]")
+      #  time.sleep(1)
+      #  print("前面に移動")
+      #if yuX[8] < yuX[6] and yuX[12] < yuX[10] and yuX[16] < yuX[14] and yuX[20] < yuX[18] and yuY[4] > yuY[0]:
+        #pg.hotkey("shift","[")
+      #  time.sleep(1)
+      #  print("背面に移動")
+      if  yuY[8] < yuY[6] and yuY[20] < yuY[18] and yuY[8] < yuY[12] and yuY[8] < yuY[16] and yuY[20] < yuY[12] and yuY[20] < yuY[16] and abs(yuX[4]-yuX[12])<13:
+        #pg.hotkey("ctrl","z")
         time.sleep(1)
-        print("前面に移動1")
-      if yuX[8] < yuX[6] and yuX[12] < yuX[10] and yuX[16] < yuX[14] and yuX[20] < yuX[18] and yuY[4] < yuY[6]:
+        print("取り消し")
+      if  yuY[8] < yuY[6] and yuY[20] < yuY[18] and yuY[8] < yuY[12] and yuY[8] < yuY[16] and yuY[20] < yuY[12] and yuY[20] < yuY[16] and 10 < np.linalg.norm(yuX[4]-yuX[10]) > 25:
+        #pg.hotkey("ctrl","shift","z")
         time.sleep(1)
-        print("前面に移動2")
+        print("元に戻す")
+      if 20 < np.linalg.norm(yuY[4]-yuY[8]) > 25 and abs(yuX[8]-yuX[12]) < 10 and abs(yuX[12]-yuX[16]) < 10 and abs(yuX[16]-yuX[20]) < 10:
+        #pg.hotkey("ctrl","c")
+        time.sleep(1)
+        print("コピー")
+      if yuY[8] < yuY[6] and yuY[12] < yuY[10] and abs(yuX[4]-yuX[14]) < 14 and yuY[16] > yuY[14] and yuY[20] > yuY[18] and 5 < np.linalg.norm(yuX[8]-yuX[12]) > 20:
+        #pg.hotkey("ctrl","v")
+        time.sleep(1)
+        print("貼り付け")
 
+      '''
       if abs(yuX[4]-yuX[20])<14 and abs(yuY[4]-yuY[20])<14:
         pg.hotkey("ctrl","shift","z")
         time.sleep(1)
         print("元に戻す")
-
+      '''
       if abs(yuX[4]-yuX[12])<12 and abs(yuY[4]-yuY[12])<12 and abs(yuX[4]-yuX[16])<12 and abs(yuY[4]-yuY[16])<12:
         t_n = time.time()
         times_n += 1
