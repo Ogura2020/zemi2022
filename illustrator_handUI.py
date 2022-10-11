a12e40a6c191d00042fed73dff3071e88f0cd65b
Ogura2020 <cocoanapoli@gmail.com>
2022-10-11 10:40:47 +0900

    ツリービューを削除しました。ボタンの設定を変更しました

--- illustrator_handUI.py
+++ illustrator_handUI.py
@@ -1,19 +1,14 @@
 import tkinter as tk
-from tkinter import font
+from PIL import Image,ImageTk
 import tkinter.ttk as ttk
 import pyautogui as pg
 import time
 import cv2
 import mediapipe as mp
-import numpy as np
 import threading
-
-def fixed_map(option):
-    return [elm for elm in style.map('Treeview', query_opt=option) if
-            elm[:2] != ('!disabled', '!selected')]
-
-stop_flag=True
-thread=None
+import numpy as np   #いらないかも！確認する！
+ 
+image_index=0
 root = tk.Tk()
 root.geometry("400x300")
 root.resizable(False, False)  #ウィンドウサイズ固定
@@ -22,23 +17,12 @@ root.configure(bg='#ECE2DB')
 root.attributes("-topmost", True)  #ウィンドウを常に一番前にする
 root.option_add('*font',("Yu Gothic UI Semibold",10))
 
-style = ttk.Style()
-style.configure("Treeview.Heading", font=("Yu Gothic UI Semibold", 10))
-style.configure("Treeview", font=("",11))
-style.map('Treeview', foreground=fixed_map('foreground'), background=fixed_map('background'))
-
-canvas=tk.Canvas(width=400,height=300, bg='#ECE2DB')
+canvas=tk.Canvas(root, bg='#1b1b1b')
 canvas.pack()
-#画像を用意
-photo1=tk.PhotoImage(file='image/ok_sign1.png')
-#画像を描画(中点x,中点y,画像)
-canvas.create_image(50,50,image=photo1)
 
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
-#t_p=100.0
-#t_n=0.0
 times_p=times_n=0.0
 a=0
 
@@ -49,12 +33,7 @@ thresh_t = 2           #2.5秒の間に指がクロスしたら
 
 cap = cv2.VideoCapture(0)
 def camera():
- #app = combobox.get()
- global stop_flag
- stop_flag = True
- t_p=100.0
- t_n=0.0
- times_n = 0
+ global image_index
  with mp_hands.Hands(
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5) as hands:
@@ -75,7 +54,7 @@ def camera():
     
     yuX = []
     yuY = []
-    if results.multi_hand_landmarks:
+    if results.multi_hand_landmarks and image_index==1:
       for hand_landmarks in results.multi_hand_landmarks:
        for i in range(21):
          yuX.append(hand_landmarks.landmark[i].x * image_width)
@@ -83,27 +62,32 @@ def camera():
          #print(hand_landmarks.landmark[4])
       X=(hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width)
       Y=(hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height)
-
+      
       if abs(yuX[4]-yuX[8])<14 and abs(yuY[4]-yuY[8])<14 and yuY[12] < yuY[8]:
-        pg.hotkey("ctrl","s")
+        #pg.hotkey("ctrl","s")
         time.sleep(1)
         print("保存")
+        text['text'] = '保存しました！'
       if abs(yuX[4]-yuX[12])<15 and abs(yuY[4]-yuY[12])<15 and abs(yuX[4]-yuX[8])<15 and abs(yuY[4]-yuY[8])<15 and yuY[16] > yuY[13] and yuY[20] > yuY[17]:
         pg.hotkey("p")
         time.sleep(1)
         print("ペンツール")
+        text['text'] = 'ペンツール'
       if yuY[8] < yuY[6] and yuY[8] < yuY[12] and yuY[8] < yuY[16] and yuY[18] < yuY[20] and abs(yuX[4]-yuX[12])<13:
-        #pg.hotkey("v")
+        pg.hotkey("v")
         time.sleep(1)
         print("選択ツール")
+        text['text'] = '選択ツール'
       if yuY[8] < yuY[6] and yuY[8] < yuY[12] and yuY[8] < yuY[16] and yuY[18] < yuY[20] and 15 < np.linalg.norm(yuX[4]-yuX[10]) > 25:
-        #pg.hotkey("v")
+        pg.hotkey("v")
         time.sleep(1)
         print("ダイレクト選択ツール")
+        text['text'] = 'ダイレクト選択ツール'
       if yuX[8] > yuX[6] and yuX[12] > yuY[10] and 10 < np.linalg.norm(yuX[4]-yuX[10]) > 20 and yuX[8] > yuX[16] and yuX[12] > yuX[16]:
-        #pg.hotkey("t")
+        pg.hotkey("t")
         time.sleep(1)
         print("文字ツール")
+        text['text'] = '文字ツール'
       #if yuX[8] < yuX[6] and yuX[12] < yuX[10] and yuX[16] < yuX[14] and yuX[20] < yuX[18] and yuY[4] < yuY[0]:
         #pg.hotkey("shift","]")
       #  time.sleep(1)
@@ -113,21 +97,25 @@ def camera():
       #  time.sleep(1)
       #  print("背面に移動")
       if  yuY[8] < yuY[6] and yuY[20] < yuY[18] and yuY[8] < yuY[12] and yuY[8] < yuY[16] and yuY[20] < yuY[12] and yuY[20] < yuY[16] and abs(yuX[4]-yuX[12])<13:
-        #pg.hotkey("ctrl","z")
+        pg.hotkey("ctrl","z")
         time.sleep(1)
         print("取り消し")
+        text['text'] = '取り消しました！'
       if  yuY[8] < yuY[6] and yuY[20] < yuY[18] and yuY[8] < yuY[12] and yuY[8] < yuY[16] and yuY[20] < yuY[12] and yuY[20] < yuY[16] and 10 < np.linalg.norm(yuX[4]-yuX[10]) > 25:
-        #pg.hotkey("ctrl","shift","z")
+        pg.hotkey("ctrl","shift","z")
         time.sleep(1)
         print("元に戻す")
+        text['text'] = 'もとに戻しました！'
       if 20 < np.linalg.norm(yuY[4]-yuY[8]) > 25 and abs(yuX[8]-yuX[12]) < 10 and abs(yuX[12]-yuX[16]) < 10 and abs(yuX[16]-yuX[20]) < 10:
-        #pg.hotkey("ctrl","c")
+        pg.hotkey("ctrl","c")
         time.sleep(1)
         print("コピー")
+        text['text'] = 'コピーしました！'
       if yuY[8] < yuY[6] and yuY[12] < yuY[10] and abs(yuX[4]-yuX[14]) < 14 and yuY[16] > yuY[14] and yuY[20] > yuY[18] and 5 < np.linalg.norm(yuX[8]-yuX[12]) > 20:
-        #pg.hotkey("ctrl","v")
+        pg.hotkey("ctrl","v")
         time.sleep(1)
         print("貼り付け")
+        text['text'] = '貼り付けました！'
 
       '''
       if abs(yuX[4]-yuX[20])<14 and abs(yuY[4]-yuY[20])<14:
@@ -135,73 +123,29 @@ def camera():
         time.sleep(1)
         print("元に戻す")
       '''
-      if abs(yuX[4]-yuX[12])<12 and abs(yuY[4]-yuY[12])<12 and abs(yuX[4]-yuX[16])<12 and abs(yuY[4]-yuY[16])<12:
-        t_n = time.time()
-        times_n += 1
-        time.sleep(0.1)
-        print("1回目",t_n)
-        if times_n == 2:
-          times_n = 0
-
-
-      if 0.1 < abs(t_n - t_p) < 1 and times_n ==1:
-        #pg.hotkey("t")
-        #time.sleep(0.5)
-        print("T")
-        
-      t_p = t_n
 
       #camera_button.config(state = "normal")
 
-def camera_on():
-  global stop_flag
-  global thread
-  if not thread:
-    #camera_button.config(state = "disabled")
-    thread_camera = threading.Thread(target=camera)
-    stop_flag=True
-    thread_camera.start()
-  print("On")
+img =[ImageTk.PhotoImage(file="image/button1.png"),ImageTk.PhotoImage(file="image/button2.png")]
+def camera_btn():
+  global image_index
+  image_index= (image_index+1) % len(img)
+  btn1 = tk.Button(root, image=img[image_index], bg="#ECE2DB", width=65, height=65, bd=0, relief="sunken", activebackground="#ECE2DB", command=camera_btn)
+  canvas.create_window(255,150,window=btn1)  #255,60
+  print("押された！",image_index)
+  if image_index==1:
+   thread_camera = threading.Thread(target=camera)
+   thread_camera.start()
+   print("On")
+  elif image_index==0:
+    print("OFF")
+    #cap.release()
+btn1 = tk.Button(root, image=img[image_index], bg="#ECE2DB", width=65,height=65, bd=0, relief="sunken", activebackground="#ECE2DB", command=camera_btn)
+canvas.create_window(255,150,window=btn1)  #255,60
+print(image_index)
 
-def camera_off():
-  global stop_flag
-  global thread
-  if thread:
-    stop_flag=False
-    thread_camera.join()
-    thread_camera=None
-  print("OFF")
-  cap.release()
-
-#test_text = tk.Label(None, text="hello World")
-#test_text.pack()
-
-camera_button = tk.Button(root, text="決 定", command=camera_on, bg="#F70B48", fg='#ffffff', width=6, font=("Yu Gothic UI Semibold",12))
-camera_button.place(x = 200, y = 37.5)
-
-camera_button2 = tk.Button(root, text="停 止", command=camera_off, bg="#F70B48", fg="#ffffff", width=6, font=("Yu Gothic UI Semibold",12))
-camera_button2.place(x = 300, y = 37.5)
-
-column = ('a', 'b', 'c')
-tree = ttk.Treeview(root, columns=column)
-tree.column('#0', width=0, stretch='no')
-tree.column('a', anchor='w', width=100)
-tree.column('b', anchor='center', width=100)
-tree.column('c',  anchor='w', width=100)
-tree.heading('#0', text='')
-tree.heading('a', text='機能', anchor='w')
-tree.heading('b', text='ジェスチャー', anchor='w')
-tree.heading('c', text='コマンド', anchor='w')
-tree.insert(parent='', index='end', values=('a', '👌', 'ctrl + z'))
-tree.place(x=20, y=100)
-  
-
-'''
-text = tk.Message(root, text="ここに選択されたアプリのショートカットジェスチャーが表示される", width=300)
-text.place(x = 50, y = 180)
-test_box = tk.Entry(width = 40)
-test_box.place(x =45, y = 100)
-'''
+text = tk.Message(root, text="○○を使用しました", width=200)
+text.place(x = 30, y = 135) #30,45
 
 def delete_window():
   cap.release()
