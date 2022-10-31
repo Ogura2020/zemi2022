b3128bfe8e1b0ff5f71fd77127b422031fdba4f4
Ogura2020 <cocoanapoli@gmail.com>
2022-11-01 01:06:41 +0900

    不必要なところを削除しました。

--- illustrator_handUI.py
+++ illustrator_handUI.py
@@ -6,7 +6,7 @@ import time
 import cv2
 import mediapipe as mp
 import threading
-import numpy as np   #いらないかも！確認する！
+import numpy as np
  
 image_index=0
 root = tk.Tk()
@@ -23,16 +23,7 @@ canvas.pack()
 photo1=tk.PhotoImage(file='image/gesture.png')
 canvas.create_image(195,500,image=photo1)
 
-mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
-
-times_p=times_n=0.0
-a=0
-
-thresh_h = 3.5            #threshold for tangent. 3.0 corresponds to 30 < theta <60. 30度～60度の間
-thresh_l = 1.0/thresh_h   #thresh_lは↑ができたら勝手に計算するので気にしなくてよし
-thresh_n = 4              #threshold for the number of points in the same direction　四回45度になったら
-thresh_t = 2           #2.5秒の間に指がクロスしたら
 
 cap = cv2.VideoCapture(0)
 def camera():
@@ -49,11 +40,7 @@ def camera():
       break
     
     image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-    #image.flags.writeable = False
     results = hands.process(image)
-
-    #image.flags.writeable = True
-    #image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     
     yuX = []
     yuY = []
@@ -63,8 +50,6 @@ def camera():
          yuX.append(hand_landmarks.landmark[i].x * image_width)
          yuY.append(hand_landmarks.landmark[i].y * image_height)
          #print(hand_landmarks.landmark[4])
-      X=(hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width)
-      Y=(hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height)
       
       if abs(yuX[4]-yuX[8])<14 and abs(yuY[4]-yuY[8])<14 and yuY[12] < yuY[8]:
         pg.hotkey("ctrl","s")
@@ -91,14 +76,6 @@ def camera():
         print("文字ツール")
         text['text'] = '文字ツール'
         time.sleep(1)
-      #if yuX[8] < yuX[6] and yuX[12] < yuX[10] and yuX[16] < yuX[14] and yuX[20] < yuX[18] and yuY[4] < yuY[0]:
-        #pg.hotkey("shift","]")
-      #  time.sleep(1)
-      #  print("前面に移動")
-      #if yuX[8] < yuX[6] and yuX[12] < yuX[10] and yuX[16] < yuX[14] and yuX[20] < yuX[18] and yuY[4] > yuY[0]:
-        #pg.hotkey("shift","[")
-      #  time.sleep(1)
-      #  print("背面に移動")
       if  yuY[8] < yuY[6] and yuY[20] < yuY[18] and yuY[8] < yuY[12] and yuY[8] < yuY[16] and yuY[20] < yuY[12] and yuY[20] < yuY[16] and abs(yuX[4]-yuX[12])<13:
         pg.hotkey("ctrl","z")
         print("取り消し")
@@ -119,7 +96,7 @@ def camera():
         print("貼り付け")
         text['text'] = '貼り付けました！'
         time.sleep(1)
-      if yuY[4] > yuY[16] and abs(yuY[4]-yuY[13])<15 and abs(yuX[4]-yuX[13])<15 and yuY[4] > yuY[8] and yuY[4] > yuY[12] and yuY[4] > yuY[16] and yuY[4] > yuY[20]:
+      if yuY[4] > yuY[16] and abs(yuY[4]-yuY[13])<16 and abs(yuX[4]-yuX[13])<16 and yuY[4] > yuY[8] and yuY[4] > yuY[12] and yuY[4] > yuY[16] and yuY[4] > yuY[20]:
         pg.keyDown("alt")
         print("拡大・縮小")
         text['text'] = '拡大・縮小'
@@ -128,14 +105,6 @@ def camera():
         pg.keyUp("alt")
         #print("解除")
 
-      '''
-      if abs(yuX[4]-yuX[20])<14 and abs(yuY[4]-yuY[20])<14:
-        pg.hotkey("ctrl","shift","z")
-        time.sleep(1)
-        print("元に戻す")
-      '''
-
-      #camera_button.config(state = "normal")
 
 #カメラ切り替えボタン
 img =[ImageTk.PhotoImage(file="image/button1.png"),ImageTk.PhotoImage(file="image/button2.png")]
@@ -151,7 +120,6 @@ def camera_btn():
    print("On")
   elif image_index==0:
     print("OFF")
-    #cap.release()
 btn1 = tk.Button(root, image=img[image_index], bg="#ECE2DB", width=90,height=90, bd=0, relief="sunken", activebackground="#ECE2DB", command=camera_btn)
 canvas.create_window(295,70,window=btn1)  
 print(image_index)
